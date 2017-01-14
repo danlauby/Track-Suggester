ed861e8324481f03044c320920f9c1454597afd8
danlauby <dmlauby@gmail.com>
2017-01-13 18:34:28 -0800

    delete 'checked' as attribute for inputs to allow branching logic and adjust conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
 $(function() {
-
-  var design = $('input[name="design"]:checked').val();
-  var largeCompany = $('input[name="large-company"]:checked').val();
-  var organizeInfo = $('input[name="organize-info"]:checked').val();
-  var startup = $('input[name="startup"]:checked').val();
-  var websites = $('input[name="websites"]:checked').val();
-  $('form#questions').submit(function(e) {
-    if (design === 'true' || largeCompany === 'false' && organizeInfo ==='false') {
+$('form#questions').submit(function(e) {
+    var design = $('input[name="design"]:checked').val();
+    var largeCompany = $('input[name="large-company"]:checked').val();
+    var organizeInfo = $('input[name="organize-info"]:checked').val();
+    var startup = $('input[name="startup"]:checked').val();
+    var websites = $('input[name="websites"]:checked').val();
+    if (design === 'true' || websites === 'true' || startup === 'true') {
       $('.design-track').toggle();
-    } else if (largeCompany === 'true' || design === 'false' && startup === 'false') {
+    } else if (largeCompany === 'true' || organizeInfo === 'true' || websites === 'true' && design === 'false') {
+      $('.php-track').toggle();
+    } else if (largeCompany === 'true' && organizeInfo === 'true' && design === 'false' || startup === 'false') {
       $('.c-sharp-track').toggle();
-    } else if (organizeInfo === 'true' || largeCompany == 'true' && design === 'false') {
-      $('php-track').toggle();
     }
     e.preventDefault();
   });
