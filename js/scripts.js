443de2034da56e5ff87f29f553032518c221fdec
danlauby <dmlauby@gmail.com>
2017-01-15 10:50:07 -0800

    add ordered list of class options and fix indentation/spacing

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,20 @@ $('form#questions').submit(function(e) {
     var startup = $('input[name="startup"]:checked').val();
     var websites = $('input[name="websites"]:checked').val();
 
-    if (!design && !largeCompany && !organizeInfo &&   !startup && !websites) {
+    if (!design && !largeCompany && !organizeInfo && !startup && !websites) {
         alert("Please answer all questions");
-      } else if (design === 'true' && largeCompany === 'true' && organizeInfo ==='true' && startup ==='true' && websites ==='true' ) {
-        $('.noMatch').append('<h2>Want \'em all huh? Sorry, I can\'t help you there!</h2>');
+      } else if (design === 'true' && largeCompany === 'true' && organizeInfo === 'true' && startup === 'true' && websites === 'true' || design === 'false' && largeCompany === 'false' && organizeInfo === 'false' && startup === 'false' && websites === 'false') {
+        $('.noMatch').append('<h1>Sorry, I can\'t help you there!</h1>');
       } else {
-      if (design === 'true' || websites === 'true' || startup === 'true') {
-        $('.design-track').slideToggle();
-      } else if (organizeInfo === 'true' || websites === 'true' && design === 'false') {
-        $('.php-track').slideToggle();
-      } else if (largeCompany === 'true') {
-        $('.c-sharp-track').slideToggle();
+        if (design === 'true' || websites === 'true' || startup === 'true') {
+          $('.design-track').slideToggle();
+        } else if (organizeInfo === 'true' || websites === 'true' && design === 'false') {
+          $('.php-track').slideToggle();
+        } else if (largeCompany === 'true') {
+          $('.c-sharp-track').slideToggle();
       } else {
         $('.noMatch').append('<h2>I think you could go with any track!</h2>');
-      }
+        }
     }
     e.preventDefault();
   });
